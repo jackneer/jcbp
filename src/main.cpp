6be727244c0aead5a20c849243af9d751258d854
Jack wu <p500.wu@gmail.com>
2019-01-30 11:36:47 +0800

    remove crow

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 #include "test.h"
-#include "crow_all.h"
 
 int main()
 {
-    crow::SimpleApp app;
-
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
-    });
-
-    // app.loglevel(crow::LogLevel::DEBUG);
-    app.port(18080).multithreaded().run();
+    say_hello();
+    say_bye();
+    return 0;
 }
